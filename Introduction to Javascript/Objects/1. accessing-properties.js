2c0ef389051d7c32ccc5d86395ae781eb548b78c
Yusri Amir <yusriamir0@gmail.com>
2024-02-09 17:00:33 +0800

    add new file

---
+++ Introduction to Javascript/Objects/1. accessing-properties.js
@@ -0,0 +1,14 @@
+let spaceship = {
+    homePlanet: "Earth",
+    color: "silver",
+    "Fuel Type": "Turbo Fuel",
+    numCrew: 5,
+    flightPath: ["Venus", "Mars", "Saturn"],
+};
+
+// Write your code below
+let crewCount = spaceship.numCrew;
+const planetArray = spaceship.flightPath;
+
+console.log(crewCount);
+console.log(planetArray);
