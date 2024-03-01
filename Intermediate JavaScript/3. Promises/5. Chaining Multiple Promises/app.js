80984a2a67a277ad13c74b46628716fc6b270b85
Yusri Amir <yusriamir0@gmail.com>
2024-03-01 23:17:44 +0800

    add new lesson

---
+++ Intermediate JavaScript/3. Promises/5. Chaining Multiple Promises/app.js
@@ -0,0 +1,35 @@
+const { checkInventory, processPayment, shipOrder } = require("./library.js");
+
+const order = {
+    items: [
+        ["sunglasses", 1],
+        ["bags", 2],
+    ],
+    giftcardBalance: 79.82,
+};
+
+// chaining promises together is called Composition
+
+firstPromiseFunction()
+    .then((firstResolveVal) => {
+        return secondPromiseFunction(firstResolveVal);
+    })
+    .then((secondResolveVal) => {
+        console.log(secondResolveVal);
+    });
+
+checkInventory(order)
+    .then((resolvedValueArray) => {
+        // Write the correct return statement here:
+        return processPayment(resolvedValueArray);
+    })
+    .then((resolvedValueArray) => {
+        // Write the correct return statement here:
+        return shipOrder(resolvedValueArray);
+    })
+    .then((successMessage) => {
+        console.log(successMessage);
+    })
+    .catch((errorMessage) => {
+        console.log(errorMessage);
+    });
