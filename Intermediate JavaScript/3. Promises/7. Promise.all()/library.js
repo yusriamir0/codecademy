e6c7a3bfde5ea7dc942cf0e6b332134b4fcda628
Yusri Amir <yusriamir0@gmail.com>
2024-03-10 22:51:28 +0800

    add new lesson

--- Intermediate JavaScript/3. Promises/7. Promise.all()/library.js
+++ Intermediate JavaScript/3. Promises/7. Promise.all()/library.js
@@ -1,12 +1,16 @@
 const checkAvailability = (itemName, distributorName) => {
-    console.log(`Checking availability of ${itemName} at ${distributorName}...`);
+    console.log(
+        `Checking availability of ${itemName} at ${distributorName}...`
+    );
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             if (restockSuccess()) {
-                console.log(`${itemName} are in stock at ${distributorName}`)
+                console.log(`${itemName} are in stock at ${distributorName}`);
                 resolve(itemName);
             } else {
-                reject(`Error: ${itemName} is unavailable from ${distributorName} at this time.`);
+                reject(
+                    `Error: ${itemName} is unavailable from ${distributorName} at this time.`
+                );
             }
         }, 1000);
     });
@@ -14,9 +18,8 @@ const checkAvailability = (itemName, distributorName) => {
 
 module.exports = { checkAvailability };
 
-
 // This is a function that returns true 80% of the time
 // We're using it to simulate a request to the distributor being successful this often
 function restockSuccess() {
-    return (Math.random() > .2);
+    return Math.random() > 0.2;
 }
