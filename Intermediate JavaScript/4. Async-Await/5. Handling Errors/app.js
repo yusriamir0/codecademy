99bbeba39c6f0c00e39b50099ff51fb0747c7563
Yusri Amir <yusriamir0@gmail.com>
2024-03-16 11:32:40 +0800

    add new lesson

---
+++ Intermediate JavaScript/4. Async-Await/5. Handling Errors/app.js
@@ -0,0 +1,15 @@
+const cookBeanSouffle = require("./library.js");
+
+// Write your code below:
+
+async function hostDinnerParty() {
+    try {
+        const dinner = await cookBeanSouffle();
+        console.log(`${dinner} is served!`);
+    } catch (error) {
+        console.log(error);
+        console.log("Ordering a pizza!");
+    }
+}
+
+hostDinnerParty();
